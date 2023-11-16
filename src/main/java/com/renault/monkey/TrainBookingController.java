95de59268f6583c6816c4df3973ac1ee03acca85
khalil.adala-extern <khalil.adala-extern@renault.com>
2023-11-16 14:58:01 +0100

    chaos monkey conf

--- src/main/java/com/renault/monkey/TrainBookingController.java
+++ src/main/java/com/renault/monkey/TrainBookingController.java
@@ -1,31 +1,43 @@
 package com.renault.monkey;
 
 
-import java.util.Arrays;
-import java.util.List;
 
 import com.renault.monkey.dto.Details;
 import com.renault.monkey.dto.TrainBookingResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class TrainBookingController {
 
-    @GetMapping("/test")
-    public List<String> test() {
-        return Arrays.asList("product1", "product2", "product3");
-    }
 
     @GetMapping("/trains")
     public TrainBookingResponse bookTrain() {
         return
                TrainBookingResponse.builder().trainReservationId("12234")
-                       .date("20/12/2023  23h23min")
+                       .date("20/12/2023  23h23min").price(999)
                        .details(Details.builder().
-                               departure("Paris").destination("ST Quentin").price(999).build())
+                               departure("Paris").destination("ST Quentin").build())
                        .build();
     }
 
-
+    @GetMapping("/trains/{status}")
+    public TrainBookingResponse bookTrain2(@PathVariable int status) {
+        if (status == 1) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Invalid request parameter");
+        } else{
+            return TrainBookingResponse.builder()
+                    .trainReservationId("12234")
+                    .date("20/12/2023  23h23min")
+                    .price(999)
+                    .details(Details.builder()
+                            .departure("Paris")
+                            .destination("ST Quentin")
+                            .build())
+                    .build();
+        }
+    }
 }
